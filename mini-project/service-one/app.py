b396a212ac2402c34069ef31a837cc16e179eb3d
Kholeo Taylor (Virtual Machine) <kholeotaylor@hotmail.co.uk>
2020-11-24 09:13:41 +0000

    initial commit

--- mini-project/service-one/app.py
+++ mini-project/service-one/app.py
@@ -1,4 +1,21 @@
-from application import app
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Gets an animal
+    animal = requests.get("http://localhost:5001/animal")
+    #Gets the noise
+    noise = requests.post("http://localhost:5001/noise", data=animal.text)
+
+    return render_template('index.html', animal=animal.text, noise=noise.text)
+
+
+
 
 if __name__ == '__main__':
-    app.run(port=5000, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
